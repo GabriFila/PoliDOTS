f9171cf37046d458c1f3d859cf747600a913a536
Gabriele Filaferro <gabriele.filaferro@gmail.com>
2020-10-25 13:59:44 +0100

    Progress on spwning multiple units based on location

---
+++ Assets/NavMeshSystem.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Entities;
+//using UnityEngine.AI;
+using Unity.Transforms;
+
+public class NavMeshSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Translation navMesh) => { }
+        );
+    }
+}
