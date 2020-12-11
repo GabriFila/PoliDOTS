9951c54a7331b5df956e2259d07e29642e1b89d8
MICHELE FILIPPINI s275902 <s275902@studenti.polito.it>
2020-12-11 21:53:35 +0100

    add onGUI method and private fields to parametrize variables

--- Assets/Scripts/UnitManager.cs
+++ Assets/Scripts/UnitManager.cs
@@ -9,12 +9,34 @@ public class UnitManager : MonoBehaviour
     public int maxIterations;
     public bool useCache;
     public int timeSlotDurationS;
-
+    public float percentageOfInfectionX100;
+    public float percentageOfInfectionWithMaskX100;
+    public float percentageOfWearingMaskX100;
+    public float infectionDistance;
+    
     public Material healthyMoveMaterial;
     public Material healthyWaitMaterial;
     public Material covidMoveMaterial;
     public Material covidWaitMaterial;
     public Mesh unitMesh;
+
+    private int totNumberOfStudents;
+    private int totNumberOfCovid;
+    private int currentSlotNumber;
+   
+    public void SetNumberOfStudents(int totNumberOfStudents)
+    {
+        this.totNumberOfStudents = totNumberOfStudents;
+    }
+
+    public void SetNumberOfCovid(int totNumberOfCovid)
+    {
+        this.totNumberOfCovid = totNumberOfCovid;
+    }
+    public void SetCurrentSlotNumber(int currentSlotNumber)
+    {
+        this.currentSlotNumber = currentSlotNumber;
+    }
 
     private void Awake()
     {
@@ -28,4 +50,15 @@ public class UnitManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    public void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 250, 25), "Students inside POLITO : " + totNumberOfStudents);
+        GUI.Box(new Rect(10, 35, 250, 25), "Exposed to COVID-19 : " + totNumberOfCovid);
+        GUI.Box(new Rect(10, 60, 250, 25), "Current timeslot : " + currentSlotNumber + "/7");
+        GUI.Box(new Rect(10, 85, 250, 25), "Risk of infection : " + percentageOfInfectionX100);
+        GUI.Box(new Rect(10, 110, 250, 25), "Risk of infection with mask : " + percentageOfInfectionWithMaskX100);
+
+        GUI.skin.box.fontSize = 15;
+    }
+
 }
